9b890238e8c230ce715db345e44faeb7ace8e86e
Marius <cr3atyve@gmail.com>
2021-11-21 11:57:51 +0200

    div

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const BasicPhrases());
@@ -26,13 +30,17 @@ class BasicPhrasesSts extends StatefulWidget {
 
 class _BasicPhrasesStsState extends State<BasicPhrasesSts> {
   final FlutterTts flutterTts = FlutterTts();
+  static TextStyle myFont =
+      GoogleFonts.robotoMono(textStyle: const TextStyle(color: Colors.white, letterSpacing: 1, fontSize: 20));
   List<Map<String, Object>> myText = <Map<String, Object>>[
     <String, Object>{'id': 0, 'name': 'Salut', 'text': 'Salut'},
     <String, Object>{'id': 1, 'name': 'Salut (Spaniola)', 'text': 'Hola'},
     <String, Object>{'id': 2, 'name': 'Ce faci?', 'text': 'Ce faci?'},
     <String, Object>{'id': 3, 'name': 'Ce faci (Spaniola)', 'text': 'Como estas?'},
-    <String, Object>{'id': 4, 'name': 'Unde este metroul', 'text': 'Unde este metroul'},
-    <String, Object>{'id': 5, 'name': 'Unde este metroul (Spaniola)', 'text': 'Donde esta el metro?'},
+    <String, Object>{'id': 4, 'name': 'Unde este metroul?', 'text': 'Unde este metroul?'},
+    <String, Object>{'id': 5, 'name': 'Unde este metroul? (Spaniola)', 'text': 'Donde esta el metro?'},
+    <String, Object>{'id': 8, 'name': 'Cat e ceasul?', 'text': 'Cât e ceasul?'},
+    <String, Object>{'id': 9, 'name': 'Cat e ceasul? (Spaniola)', 'text': 'Que hora es?'},
     <String, Object>{'id': 6, 'name': 'La revedere', 'text': 'La revedere'},
     <String, Object>{'id': 7, 'name': 'La revedere (Spaniola)', 'text': 'Hasta la vista beiby'}
   ];
@@ -59,26 +67,29 @@ class _BasicPhrasesStsState extends State<BasicPhrasesSts> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Basic Phrases'),
+        backgroundColor: Colors.amber,
       ),
       body: GridView.builder(
-          padding: const EdgeInsets.all(20),
+          padding: const EdgeInsets.all(10),
           gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 200, childAspectRatio: 3 / 2, crossAxisSpacing: 20, mainAxisSpacing: 20),
+              maxCrossAxisExtent: 200, childAspectRatio: 3 / 2, crossAxisSpacing: 10, mainAxisSpacing: 10),
           itemCount: myText.length,
           itemBuilder: (BuildContext ctx, int index) {
             return GestureDetector(
               child: Container(
                 alignment: Alignment.center,
                 decoration: BoxDecoration(
-                  gradient: LinearGradient(
+                  gradient: const LinearGradient(
                       begin: Alignment.bottomLeft,
                       end: Alignment.topRight,
-                      colors: <Color>[Colors.amber, Colors.amber.shade100]),
+                      colors: <Color>[Colors.amber, Colors.blueGrey]),
                   borderRadius: BorderRadius.circular(10),
                 ),
                 child: Text(
                   myText[index]['name'].toString(),
                   textAlign: TextAlign.center,
+                  style: myFont,
+                  // TextStyle(color: Colors.white, fontSize: 20.0, fontFamily: 'Teko'),
                 ),
               ),
               onTap: () {
